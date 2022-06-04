0046b6f901046b7e47a4b5ae26d1a18ecfa297de
Abinet Tassew <39063284+abinet1@users.noreply.github.com>
2022-06-04 10:27:06 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
 function Square(props) {
     return (
         <button className="square" onClick={props.onClick}>
